a443af93df852192d2c6e2064917900d99ed396f
brymlee <brymlee1911@gmail.com>
2022-02-24 20:22:08 -0700

    Little better

--- src/Data/Set.hs
+++ src/Data/Set.hs
@@ -1,10 +1,24 @@
-module Data.Set (set, Set, (<#>), (<$!>), fmapSet, (<!>), associative, (>>=!), sequence', (|>), add, (<|), remove) where
+module Data.Set (Set, set, (<|), (|>)) where
 
 import Data.List (sort)
 
 newtype Set a = Set [a] deriving (Show, Eq, Ord)
 
-type Constraint a = (Eq a, Ord a)
+instance Functor Set where
+    f `fmap` (Set a) = Set $ f <$> a
+
+instance Applicative Set where
+    pure a = Set [a]
+    (Set f) <*> (Set a) = Set $ f <*> a
+
+instance Monad Set where
+    return = pure
+    (Set a) >>= f = foldr (\ (Set a) (Set b) -> Set (a <> b)) (Set []) $ f <$> a
+
+instance Constraint a => Semigroup (Set a) where
+    (Set a) <> (Set b) = Set $ dedup $ a <> b
+
+type Constraint a = (Show a, Eq a, Ord a)
 
 dedup :: Constraint a => [a] -> [a]
 dedup a = dedup_ (sort a) []
@@ -17,41 +31,17 @@ dedup__ :: Constraint a => [a] -> a -> Bool -> [a]
 dedup__ a _ True = a
 dedup__ a b False = a <> [b]
 
-set :: Constraint a => [a] -> Set a
-set a = Set $ dedup a
-
-infixl 4 <#>
-(<#>) :: (Functor f) => f a -> (a -> b) -> f b
-(<#>) = fmap'
-fmap' :: (Functor f) => f a -> (a -> b) -> f b
-a `fmap'` f = f <$> a
-
-infixl 4 <$!>
-(<$!>) :: (Constraint a, Constraint b) => (a -> b) -> Set a -> Set b
-(<$!>) = fmapSet
-fmapSet :: (Constraint a, Constraint b) => (a -> b) -> Set a -> Set b
-f `fmapSet` (Set a) = set $ a <#> \ b -> f b
-
-infixr 6 <!>
-(<!>) :: (Constraint a) => Set a -> Set a -> Set a
-(<!>) = associative
-associative :: (Constraint a) => Set a -> Set a -> Set a
-associative (Set a) (Set b) = set $ a <> b
-
-infixl 1 >>=!
-(>>=!) :: (Constraint a, Constraint b) => Set a -> (a -> Set b) -> Set b
-(>>=!) = sequence'
-sequence' :: (Constraint a, Constraint b) => Set a -> (a -> Set b) -> Set b
-(Set a) `sequence'` f = foldr (\ a b -> a <!> b) (set []) $ (\ b -> f b) <$> a
-
 infixl 2 |>
 (|>) :: Constraint a => Set a -> a -> Set a
 (|>) = add
 add :: Constraint a => Set a -> a -> Set a
-a `add` b = a <!> set [b]
+a `add` b = a <> set [b]
 
 infixl 2 <|
-(<|) :: Constraint a => Set a -> a -> Set a
+(<|) :: Constraint a => Set a -> a -> Set a 
 (<|) = remove
 remove :: Constraint a => Set a -> a -> Set a
 (Set a) `remove` b = set $ (\ c -> b /= c) `filter` a
+
+set :: Constraint a => [a] -> Set a
+set a = Set $ dedup a
--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,6 @@
 module Main where 
 
-import Data.Set (set, Set, (<$!>), (<!>), (>>=!), (|>), (<|))
+import Data.Set (Set, set, (|>), (<|))
 
 assert :: String -> Bool -> IO()
 assert _ True = putStr ""
@@ -10,7 +10,7 @@ main :: IO()
 main = do
     assert "Two set constructions should equal each other" $ (set [1, 2, 3]) == (set [1, 2 ,3])
     assert "A set constructed with duplicates should remove them after" $ (set [1, 1, 2, 2, 3, 3]) == (set [1, 2, 3])
-    assert "A set should be fmappable" $ (set [2, 3, 4]) == ((\ a -> a + 1) <$!> (set [1, 2, 3]))
-    assert "A set should be associatable" $ (set [1, 2, 3]) == ((set [1, 2, 3]) <!> (set [1, 2, 3]))
-    assert "A set should be work pretty much like a Monad" $ (set [2, 3, 4]) == ((set [1, 2, 3]) >>=! (\ a -> set [(a + 1)]))
+    assert "A set should be fmappable" $ (set [2, 3, 4]) == ((\ a -> a + 1) <$> (set [1, 2, 3]))
+    assert "A set should be associatable" $ (set [1, 2, 3]) == ((set [1, 2, 3]) <> (set [1, 2, 3]))
+    assert "A set should be work pretty much like a Monad" $ (set [2, 3, 4]) == ((set [1, 2, 3]) >>= (\ a -> set [(a + 1)]))
     assert "A set can be added to and removed one at a time" $ (==) (set [1, 2, 4]) $ (set [] :: Set Int) |> 4 |> 1 |> 2 |> 2 |> 3 <| 3 
